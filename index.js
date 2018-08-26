403bc43911c1d6040c357b6b49c4aa31c179b1de
mmetros@wesleyan.edu <mmetros@wesleyan.edu>
2018-08-26 12:06:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,16 @@ function nestedTarget(){
   }
 }
 
-function increa
+function increaseRankBy(n) {
+  // grab all the li with the parent class of rankedLists
+  var rankedLists = document.querySelectorAll(".ranked-list");
+  var i = 0;
+  while (rankedLists[i]) {
+    var sibling = rankedLists
+    for (var p = 0; p < rankedLists[i].childElementCount; p++) {
+      // we want it to access the child and increase by one
+      rankedLists[i].children[p].innerHTML = parseInt(rankedLists[i].children[p].innerHTML) + 1
+    }
+    i++
+  }
+}
