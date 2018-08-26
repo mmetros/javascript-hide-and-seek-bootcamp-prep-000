43ef888a4b546bfa90403a29c485da2c8c8fec2c
mmetros@wesleyan.edu <mmetros@wesleyan.edu>
2018-08-26 11:27:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function getFirstSelector(selector){
+  return document.querySelector(`${selector}`)
+}
 
+function nestedTarget(){
+  var nestedID = document.getElementById("nested");
+  var node = nestedID
+  while(node){
+    if(node.classList.contains("target")){
+      return node
+    } else {
+      node = node.firstElementChild
+    }
+  }
+}
+
+function increa
